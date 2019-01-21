ac338b9551ee80525c6fb8d3777c4a8203f92cb3
tqhdesilva <tdesilvaqihao@gmail.com>
2019-01-21 10:32:44 -0700

    slice on last parenthesis

--- tests/test_pydbg.py
+++ tests/test_pydbg.py
@@ -23,12 +23,21 @@ def test_variables():
         dbg(strType)
         dbg(boolType)
         dbg(NoneType)
+        dbg(add(1, 2))
 
     want = f"""[{cwd}/tests/test_pydbg.py:21] intType = 2
 [{cwd}/tests/test_pydbg.py:22] floatType = 2.1
 [{cwd}/tests/test_pydbg.py:23] strType = mystring
 [{cwd}/tests/test_pydbg.py:24] boolType = True
 [{cwd}/tests/test_pydbg.py:25] NoneType = None
+[{cwd}/tests/test_pydbg.py:26] add(1, 2) = 3
 """
+    outstr = out.getvalue()
+    print(outstr)
+    import pdb; pdb.set_trace()
+    assert outstr  == want
 
-    assert out.getvalue() == want
+
+def add(x, y):
+    return x + y
+
